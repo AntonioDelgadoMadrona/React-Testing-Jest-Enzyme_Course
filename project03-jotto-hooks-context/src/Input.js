c65cae56f1b42b1d37ba242fe18f29f12890675d
tonidelgadomadrona <50912526+tonidelgadomadrona@users.noreply.github.com>
2020-05-11 18:58:25 +0200

    See you tomorrow

--- project03-jotto-hooks-context/src/Input.js
+++ project03-jotto-hooks-context/src/Input.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const Input = () => {
+const Input = props => {
+
+    const { secretWord } = props;
+    
     return (
         <div data-test="component-input">
             
         </div>
     )
 };
+
+Input.proTypes = {
+    secretWord: PropTypes.string.isRequired,
+}
 
 export default Input;
--- project03-jotto-hooks-context/src/Input.test.js
+++ project03-jotto-hooks-context/src/Input.test.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
 import { shallow } from 'enzyme';
-import { findByTestAttr } from '../test/testUtils';
+import { findByTestAttr, checkProps } from '../test/testUtils';
 
 import Input from './Input';
 
+/**
+ * Setup function for input component 
+ * @returns {ShallowWrapper}
+ */
+const setup = (secretword = 'party') => {
+    return shallow(<Input secretword={secretword} />)
+}
+
 test('should render without errors', () => {
-    const wrapper = shallow(<Input />);
+    const wrapper = setup();
     const component = findByTestAttr(wrapper, 'component-input');
     expect(component).toBeTruthy();
+})
+
+test('should not throw warning with expected props', () => {
+    checkProps(Input, { secretword: 'party' });
 })
